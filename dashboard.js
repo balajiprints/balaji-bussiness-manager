741a662603c5add4a4307ad95207c0d2adb40d18
balajiprints <balajiemitrachuru001@gmail.com>
2026-06-18 23:33:35 +0530

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,64 +1,20 @@
-// Balaji Business Manager Dashboard
-
 function calculateDashboard() {
 
   const data = getData();
 
-  // Income Total
-  let incomeTotal = data.income.reduce(
-    (sum, item) => sum + Number(item.amount || 0),
-    0
-  );
+  let incomeTotal = 0;
+  let expenseTotal = 0;
 
-  // Expense Total
-  let expenseTotal = data.expenses.reduce(
-    (sum, item) => sum + Number(item.amount || 0),
-    0
-  );
+  data.income.forEach(item => {
+    incomeTotal += Number(item.amount || 0);
+  });
 
-  // Profit
+  data.expenses.forEach(item => {
+    expenseTotal += Number(item.amount || 0);
+  });
+
   let profit = incomeTotal - expenseTotal;
 
-  // Accounts Total
-  let accountTotal = data.accounts.reduce(
-    (sum, item) => sum + Number(item.balance || 0),
-    0
-  );
-
-  // IDs Total
-  let idTotal = data.ids.reduce(
-    (sum, item) => sum + Number(item.balance || 0),
-    0
-  );
-
-  // Cash Total
-  let cashTotal =
-    Number(data.cash.shop || 0) +
-    Number(data.cash.galla || 0) +
-    Number(data.cash.coins || 0) +
-    Number(data.cash.home || 0);
-
-  // UPI Total
-  let upiTotal = data.upi.reduce(
-    (sum, item) => sum + Number(item.amount || 0),
-    0
-  );
-
-  // Udhaar Total
-  let udhaarTotal = data.udhaar.reduce(
-    (sum, item) => sum + Number(item.amount || 0),
-    0
-  );
-
-  // Business Worth
-  let businessWorth =
-    accountTotal +
-    idTotal +
-    cashTotal +
-    upiTotal +
-    udhaarTotal;
-
-  // Update Dashboard
   document.getElementById("todayIncome").innerText =
     "₹" + incomeTotal;
 
@@ -67,25 +23,6 @@ function calculateDashboard() {
 
   document.getElementById("todayProfit").innerText =
     "₹" + profit;
-
-  document.getElementById("totalAccounts").innerText =
-    "₹" + accountTotal;
-
-  document.getElementById("totalIds").innerText =
-    "₹" + idTotal;
-
-  document.getElementById("totalCash").innerText =
-    "₹" + cashTotal;
-
-  document.getElementById("totalUpi").innerText =
-    "₹" + upiTotal;
-
-  document.getElementById("totalUdhaar").innerText =
-    "₹" + udhaarTotal;
-
-  document.getElementById("businessWorth").innerText =
-    "₹" + businessWorth;
 }
 
-// Run Dashboard
 calculateDashboard();
